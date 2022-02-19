d5bb29070bbb2562f5f0e5e5a57d15216c183577
peruibeloko <cmendonca32@gmail.com>
2022-02-19 18:37:59 -0300

    setup, background property not working

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>header stuff</header>
+
+  <main></main>
+
+  <footer>footer stuff</footer>
+</template>
+
+<script setup></script>
+
+<style>
+@import './assets/theme.css';
+</style>
