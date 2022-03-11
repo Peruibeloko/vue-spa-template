726bf06b186c7d4e602f301726ddb9f854bd16ce
peruibeloko <cmendonca32@gmail.com>
2022-03-11 00:20:04 -0300

    add default text input and button styling

---
+++ src/components/TextInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="input-wrapper">
+    <label>
+      {{ props.label }}
+    </label>
+    <input
+      class="input"
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['label', 'modelValue']);
+defineEmits(['update:modelValue']);
+</script>
+
+<style scoped></style>
